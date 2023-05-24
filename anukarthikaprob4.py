b94919134a94703ddde337d94e5f13b9fe2105b7
Anukarthika-S <130376645+Anukarthika-S@users.noreply.github.com>
2023-05-24 10:21:56 +0530

    Create anukarthikaprob4.py

---
+++ anukarthikaprob4.py
@@ -0,0 +1,8 @@
+#program tofind positive or negative number
+num=float(input("enter a number:"))
+if(num>0):
+  print("it is a positive number")
+elif (num<0):
+  print("it is a negative number")
+else:
+  ptint("invalid!")
