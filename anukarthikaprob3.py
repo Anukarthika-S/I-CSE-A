edf5d56882ec9199aa6953e2bbefe9820047b971
Anukarthika-S <130376645+Anukarthika-S@users.noreply.github.com>
2023-05-24 10:17:58 +0530

    Update anukarthikaprob3.py

--- anukarthikaprob3.py
+++ anukarthikaprob3.py
@@ -1,5 +1,6 @@
 #program to calculate the wind chill index
 temperature=float(input("enterthe temperature in degrees"))
 velocity=float(input("enter the windspeed in kms per hour"))
-chill_index=13.12+(0.6215*temperature)-(11.37*(v**0.16))+(0.3965*t*(v**0.16))
-print("the wind chill index for the given data is:",chill_index)
+chillIndex=13.12+(0.6215*temperature)-(11.37*(v**0.16))+(0.3965*t*(v**0.16))
+chillIndex1=chillIndex//1
+print("the wind chill index for the given data is:",chillIndex1)
