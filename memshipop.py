1b93b48b1cb61b9cdb99e03bfd0a9cfc9e92bbf5
Anukarthika-S <130376645+Anukarthika-S@users.noreply.github.com>
2023-07-18 19:45:47 +0530

    Create memshipop.py

---
+++ memshipop.py
@@ -0,0 +1,14 @@
+#in operator
+str1="Hello Anu!!!"
+str2="Anu"
+if str2 in str1:
+    print("yes")
+else:
+    print("no")
+#not in operator
+    str1="Hello Anu!!!"
+str2="Anu"
+if str2 not in str1:
+    print("no")
+else:
+    print("yes")
