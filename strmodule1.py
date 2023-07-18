2e7d13ea0cd3dbaa28ee8fb4d02ed48e7579c101
Anukarthika-S <130376645+Anukarthika-S@users.noreply.github.com>
2023-07-18 19:16:18 +0530

    Create strmodule1.py

---
+++ strmodule1.py
@@ -0,0 +1,7 @@
+msg="Hello World!"
+print("lower=",msg.lower())
+print("upper=",msg.upper())
+print("split=",msg.split())
+print("join=",'-'.join(msg.split()))
+print("find hello=",msg.find("Hello"))
+print("count=",msg.count("o"))
